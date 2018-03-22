5accb1b2992dc2f62d7e7e6e2c973b9a9075f225
jackie <jcqnly@gmail.com>
2018-03-22 16:15:16 -0700

    add read-on click function

--- aaron-jackie/scripts/articleView.js
+++ aaron-jackie/scripts/articleView.js
@@ -73,19 +73,27 @@ articleView.handleCategoryFilter = function() {
 
 articleView.handleMainNav = function() {
   // TODO: Add an event handler to .main-nav elements that will power the Tabs feature.
-  
+  $('nav [data-content = "about"]').on('click', function(){
+    $('article').hide();
+  });
   // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
+
   // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
 
   // REVIEW: Now trigger a click on the first .tab element, to set up the page.
-  $('.main-nav .tab:first').click();
+  $('.main-nav .tab:first').on('click', function() {
+    $('article').show();
+  });
 };
 
 articleView.setTeasers = function() {
   // REVIEW: Hide elements beyond the first 2 in any article body.
   $('.article-body *:nth-of-type(n+2)').hide();
-
   // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
+  $('.read-on').on('click', function(){
+    event.preventDefault();
+    $('.article-body *:nth-of-type(n+2)').show();
+  });
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
 };
 
@@ -94,4 +102,6 @@ $(document).ready(function() {
   articleView.populateFilters();
   articleView.handleAuthorFilter();
   articleView.handleCategoryFilter();
+  articleView.handleMainNav();
+  articleView.setTeasers();
 })
